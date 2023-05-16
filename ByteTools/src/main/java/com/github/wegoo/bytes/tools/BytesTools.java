6ecaf4600e5517d556c1621cbf1cafe1688ae2b9
zhenwei1108 <zhenwei1108@163.com>
2023-05-16 09:17:07 +0800

    add tools

--- ByteTools/src/main/java/com/github/wegoo/bytes/tools/BytesTools.java
+++ ByteTools/src/main/java/com/github/wegoo/bytes/tools/BytesTools.java
@@ -1,5 +1,7 @@
 package com.github.wegoo.bytes.tools;
 
+import java.lang.reflect.Array;
+
 /**
  * @author: zhangzhenwei
  * @description:
@@ -7,5 +9,27 @@ package com.github.wegoo.bytes.tools;
  * @since: 1.0.0
  */
 public class BytesTools {
+
+  /**
+   * @param [data]
+   * @return byte[]
+   * @author zhangzhenwei
+   * @description merge 合并byte数组 时间和内存，我选时间
+   * @date 2023/5/16  09:10
+   * @since: 1.0.0
+   */
+  public static byte[] merge(byte[]... data) {
+    byte[] result = new byte[0];
+    if (data != null) {
+      for (byte[] datum : data) {
+        byte[] mediaValue = new byte[result.length + datum.length];
+        System.arraycopy(result, 0, mediaValue, 0, result.length);
+        System.arraycopy(datum, 0, mediaValue, result.length, datum.length);
+        result = mediaValue;
+      }
+    }
+    return result;
+  }
+
 
 }
